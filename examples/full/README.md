09778e93e7db2bd395b9d1209dcfe21f55f7165a
Steven Nemetz <snemetz@wiser.com>
2018-03-27 13:11:37 -0700

    Add full example - not tested yet

---
+++ examples/full/README.md
@@ -0,0 +1,3 @@
+# Example: full
+
+Full example of adding members to organization, creating teams, and add members to teams
---
+++ examples/full/main.tf
@@ -0,0 +1,54 @@
+module "org-members" {
+  source = "devops-workflow/members/github"
+  version = "0.0.1"
+
+  providers = {
+    github = "github.devops"
+  }
+
+  users = [
+    {
+      username = "user1"
+    },
+    {
+      username = "user2"
+      role     = "owner"
+    },
+  ]
+}
+
+module "team" {
+  source  = "devops-workflow/teams/github"
+  version = "0.0.1"
+
+  providers = {
+    github = "github.devops"
+  }
+
+  teams = [
+    {
+      name        = "team-test"
+      description = "Team for Terraform testing"
+    },
+  ]
+}
+
+module "team-members" {
+  source     = "../../"
+  depends_on = ["module.org-members", "module.team"]
+  team_id    = "${element(module.team.ids, 0)}"
+
+  providers = {
+    github = "github.devops"
+  }
+
+  users = [
+    {
+      username = "user1"
+    },
+    {
+      username = "user2"
+      role     = "maintainer"
+    },
+  ]
+}
